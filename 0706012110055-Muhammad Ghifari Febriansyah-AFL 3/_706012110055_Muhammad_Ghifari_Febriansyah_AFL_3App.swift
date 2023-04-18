5e1917232fef5da61cd699b613607e6d50803cc6
GhifhariFebri <suriansyahghifari@gmail.com>
2023-04-18 08:29:01 +0700

    Initial Commit

---
+++ 0706012110055-Muhammad Ghifari Febriansyah-AFL 3/_706012110055_Muhammad_Ghifari_Febriansyah_AFL_3App.swift
@@ -0,0 +1,17 @@
+//
+//  _706012110055_Muhammad_Ghifari_Febriansyah_AFL_3App.swift
+//  0706012110055-Muhammad Ghifari Febriansyah-AFL 3
+//
+//  Created by MacBook Pro on 18/04/23.
+//
+
+import SwiftUI
+
+@main
+struct _706012110055_Muhammad_Ghifari_Febriansyah_AFL_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
