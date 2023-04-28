3db659f8da226f92a0d47a9303a428627d8c93fa
GhifhariFebri <suriansyahghifari@gmail.com>
2023-04-28 08:11:23 +0700

    All Chapter Done, Just need command

--- 0706012110055-Muhammad Ghifari Febriansyah-AFL 3/Views/Landmarks/LandmarkList.swift
+++ 0706012110055-Muhammad Ghifari Febriansyah-AFL 3/Views/Landmarks/LandmarkList.swift
@@ -11,36 +11,38 @@ import SwiftUI
 struct LandmarkList: View {
     @EnvironmentObject var modelData: ModelData
     @State private var showFavoritesOnly = false
+    @State private var filter = FilterCategory.all
+    @State private var selectedLandmark: Landmark?
+    
+    enum FilterCategory: String, CaseIterable, Identifiable {
+        case all = "All"
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains = "Mountains"
+
+        var id: FilterCategory { self }
+    }
 
     //showing each landmark in the landmarkdata
     var filteredLandmarks: [Landmark] {
         modelData.landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
+            && (filter == .all || filter.rawValue == landmark.category.rawValue)
         }
     }
-
+    var title: String {
+          let title = filter == .all ? "Landmarks" : filter.rawValue
+          return showFavoritesOnly ? "Favorite \(title)" : title
+      }
+    
+    var index: Int? {
+        modelData.landmarks.firstIndex(where: { $0.id == selectedLandmark?.id })
+    }
+    
     var body: some View {
-//        NavigationView {
-//            //List view
-//            List {
-//                //toggling on show favorites only
-//                Toggle(isOn: $showFavoritesOnly) {
-//                    Text("Favorites only")
-//                }
-//                //put navigation in each landmark
-//                ForEach(filteredLandmarks) { landmark in
-//                    NavigationLink {
-//                        LandmarkDetail(landmark: landmark)
-//                    } label: {
-//                        LandmarkRow(landmark: landmark)
-//                    }
-//                }
-//            }
-//            //setting title for list
-//            .navigationTitle("Landmarks")
-//        }
-        NavigationStack{
-            List {
+        NavigationView {
+            //List view
+            List(selection: $selectedLandmark) {
                 //toggling on show favorites only
                 Toggle(isOn: $showFavoritesOnly) {
                     Text("Favorites only")
@@ -52,12 +54,33 @@ struct LandmarkList: View {
                     } label: {
                         LandmarkRow(landmark: landmark)
                     }
+                    .tag(landmark)
                 }
             }
             //setting title for list
-            .navigationTitle("Landmarks")
-            
+            .navigationTitle(title)
+            .frame(minWidth: 300)
+            .toolbar {
+                ToolbarItem {
+                    Menu {
+                        Picker("Category", selection: $filter) {
+                            ForEach(FilterCategory.allCases) { category in
+                                    Text(category.rawValue).tag(category)
+                                        }
+                                }
+                    .pickerStyle(.inline)
+
+                    Toggle(isOn: $showFavoritesOnly) {
+                    Label("Favorites only", systemImage: "star.fill")
+                                              }
+                    } label: {
+                        Label("Filter", systemImage: "slider.horizontal.3")
+                        }
+                    }
+               }
+            Text("Select a Landmark")
             }
+        .focusedValue(\.selectedLandmark, $modelData.landmarks[index ?? 0])
         }
     }
 
---
+++ MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,35 @@
+//
+//  LandmarkCommands.swift
+//  0706012110055-Muhammad Ghifari Febriansyah-AFL 3
+//
+//  Created by MacBook Pro on 28/04/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    @FocusedBinding(\.selectedLandmark) var selectedLandmark
+    
+    var body: some Commands {
+        SidebarCommands()
+        
+        CommandMenu("Landmark") {
+            Button("\(selectedLandmark?.isFavorite == true ? "Remove" : "Mark") as Favorite") {
+                selectedLandmark?.isFavorite.toggle()
+            }
+            .keyboardShortcut("f", modifiers: [.shift, .option])
+            .disabled(selectedLandmark == nil)
+        }
+    }
+}
+private struct SelectedLandmarkKey: FocusedValueKey {
+    typealias Value = Binding<Landmark>
+}
+
+extension FocusedValues {
+    var selectedLandmark: Binding<Landmark>? {
+        get { self[SelectedLandmarkKey.self] }
+        set { self[SelectedLandmarkKey.self] = newValue }
+    }
+}
